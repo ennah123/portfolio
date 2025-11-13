7b3ca34f52ca87e4cd7d383d9ec656ccb7ce757f
ennah123 <123ennah@gmail.com>
2025-11-13 02:00:46 +0100

    add education and techStack to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
-import AbouteMe from "./components/abouteMe";
-import Hero from "./components/hero";
+import AbouteMe from "./components/AbouteMe";
+import Education from "./components/Education";
+import Hero from "./components/HeroSection";
+import TechStack from "./components/TechStack";
 
 export default function Home() {
   return (
      <div className="w-full ">
       <Hero />
       <AbouteMe/>
+      <Education/>
+      <TechStack/>
     </div>
   );
 }
